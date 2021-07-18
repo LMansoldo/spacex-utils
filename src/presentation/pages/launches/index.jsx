251e3b06b6c98426a1d964b37f4ccf282b90c2d4
LMansoldo <lucasmansoldo0@gmail.com>
2021-07-18 20:03:16 -0300

    feat: inserting query for launches data

--- src/presentation/pages/launches/index.jsx
+++ src/presentation/pages/launches/index.jsx
@@ -1,17 +1,55 @@
+/* eslint-disable no-console */
 import React from 'react';
+import { gql, useQuery } from '@apollo/client';
 import { Card } from '../../components';
 
-const Launches = () => (
-	<div>
-		<Card
-			name="name"
-			rocket="rocket"
-			place="place"
-			date="date"
-			description="description"
-			shipList="shipList"
-		/>
-	</div>
-);
+const Launches = () => {
+	const GET_LAUNCHES = gql`
+		query GetLaunchList {
+			launchesPast(limit: 10) {
+				id
+				mission_name
+				launch_date_local
+				links {
+					article_link
+					video_link
+				}
+				launch_site {
+					site_name_long
+				}
+				rocket {
+					rocket_name
+				}
+				ships {
+					name
+					home_port
+					image
+				}
+			}
+		}
+	`;
+
+	const { data, loading, error } = useQuery(GET_LAUNCHES);
+
+	if (loading) return <span>loading...</span>;
+	if (error || !data) return <p>ERROR</p>;
+
+	return (
+		<div>
+			{data.launchesPast &&
+				data.launchesPast.map((launch) => (
+					<Card
+						key={`spcx-launch-${launch.id}`}
+						name={launch.name}
+						rocket={launch.rocket.rocket_name}
+						place={launch.launch_site.site_name_long}
+						date={launch.launch_date_local}
+						description="description"
+						shipList={launch.ships}
+					/>
+				))}
+		</div>
+	);
+};
 
 export default Launches;
