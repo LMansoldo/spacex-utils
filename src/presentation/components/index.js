46ddc5df0953ea6402696f06ad6f0ff87f447be7
LMansoldo <lucasmansoldo0@gmail.com>
2021-07-19 09:07:35 -0300

    feat: inserting card in rockets page

--- src/presentation/components/index.js
+++ src/presentation/components/index.js
@@ -3,4 +3,5 @@ export { default as Button } from './Button';
 export { default as Menu } from './Menu';
 export { default as Head } from './Head';
 export { default as Card } from './Card';
+export { default as RocketCard } from './RocketCard';
 export { default as TextField } from './TextField';
